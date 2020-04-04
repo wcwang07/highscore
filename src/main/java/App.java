6c9f3bd8f58fba90b937490e0144d1fe115270dd
wwang13 <weiching_wang@intuit.com>
2020-04-04 15:40:13 -0700

    version 1

---
+++ src/main/java/App.java
@@ -0,0 +1,99 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class App {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+        public static void main(String[] args) throws FileNotFoundException {
+            //assume we are dealing with text files
+            Map<Integer, Object> rawTable = new HashMap<>();
+            LinkedHashMap<Integer, Object> reverseSortedMap = new LinkedHashMap<>();
+            FileReader fileReader = new FileReader(args[0]);
+            try(BufferedReader br = new BufferedReader(fileReader)) {
+                String line = br.readLine();
+                while (line != null) {
+                    try {
+                        String[] idWithPayload = line.split(":", 2);
+                        //Assume Scores are non-negative 32-bit integers.
+                        if (idWithPayload[0] != null) {
+                            JsonObject result = JsonParser.parseString(idWithPayload[1])
+                                .getAsJsonObject();
+                            //Scores can repeat, but you should only count the `id` of the _last_ line processed as the "winning" `id`, override with latest entry
+                            rawTable.put(Integer.valueOf(idWithPayload[0]), result);
+                        }
+
+                    } catch (JsonSyntaxException e) {
+                        System.out.println("invalid json format No JSON object could be decoded\n"
+                            + " THIS IS NOT JSON");
+                        System.exit(2);
+                    }
+                    // Read next line
+                    line = br.readLine();
+                }
+            }
+            catch (Exception e){
+                LOGGER.error(e.getMessage());
+            }
+            rawTable.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+                .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+            LOGGER.debug(reverseSortedMap.toString());
+            List<Map<String, Object>> res = returndResultByHighest(reverseSortedMap, Integer.valueOf(args[1]));
+            Gson gsonOut = new GsonBuilder().setPrettyPrinting().create();
+            System.out.println(gsonOut.toJson(res));
+
+    }
+
+    public static List<Map<String,Object>> returndResultByHighest(Map<Integer, Object> sortedInput, int highest) {
+
+        boolean idAtRootLevel = false;
+        int numOfAddedRecord = 0;
+        List resultList = new ArrayList<Map<String,Object>>();
+
+        for(Entry<Integer, Object> row : sortedInput.entrySet()){
+            try {
+                Map<String,Object> record = new Gson().fromJson((JsonElement) row.getValue(), Map.class);
+                LOGGER.debug("Current record:" +record + " numOfAddedRecord "+numOfAddedRecord+ "entry size: "+sortedInput.size());
+                //The `id` must be at the root level of the JSON object,
+                //traverse each root nodes
+                for(String nodeKey:record.keySet()){
+                    //traverse root level node to determine if `id` is present
+                    if(nodeKey.equalsIgnoreCase("id")) idAtRootLevel=true;
+
+                }
+                if(idAtRootLevel && numOfAddedRecord<highest){
+                    //add from the highest score
+                    resultList.add(record);
+                    numOfAddedRecord++;
+                }
+                //reset idAtRootLevel flag
+                idAtRootLevel=false;
+            }
+            catch (Exception e){
+               LOGGER.debug("ERROR :"+e);
+            }
+        }
+        //reverse list because highest score is pushed last
+        Collections.reverse(resultList);
+        return resultList;
+    }
+}
---
+++ src/test/java/FileScoreReaderTest.java
@@ -0,0 +1,105 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import java.security.SecureRandom;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.google.gson.JsonObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FileScoreReaderTest {
+
+
+  private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+  private static final String NUMBER = "0123456789";
+
+  private static final String DATA_FOR_RANDOM_STRING = CHAR_LOWER + NUMBER;
+  private static SecureRandom random = new SecureRandom();
+
+  @Test
+  public void testReturndResultHWithHighest2HappyPath() {
+
+
+    HashMap<Integer, Object> record = new HashMap<Integer, Object>();
+
+    //insert determined scores and its payload
+    GsonBuilder gsonMapBuilder = new GsonBuilder();
+    Gson gsonObject = gsonMapBuilder.create();
+
+    String json1 = gsonObject.toJson(generateRandomPayload());
+    String json2 = gsonObject.toJson(generateRandomPayload());
+    String json3 = gsonObject.toJson(generateRandomPayload());
+
+    JsonObject jsonObject1 = JsonParser.parseString(json1)
+        .getAsJsonObject();
+    JsonObject jsonObject2 = JsonParser.parseString(json2)
+        .getAsJsonObject();
+    JsonObject jsonObject3 = JsonParser.parseString(json3)
+        .getAsJsonObject();
+
+    record.put(14027069,jsonObject1);
+    record.put(15027069,jsonObject2);
+    record.put(16027069,jsonObject3);
+
+    List<Map<String,Object>> result = App.returndResultByHighest(record,2);
+    Map<String, Object> t1 = result.get(0);
+    Assert.assertEquals(jsonObject3.get("id").getAsString(), t1.get("id"));
+
+  }
+
+
+  private HashMap generateRandomPayload() {
+
+    HashMap<String, Object> map = new HashMap<>();
+    SecureRandom random = new SecureRandom();
+//    String uuid = UUID.randomUUID().toString();
+    int umbrella = random.nextInt(10000);
+    int value = random.nextInt(100000);
+
+    int minDay = (int) LocalDate.of(1900, 1, 1).toEpochDay();
+    int maxDay = (int) LocalDate.of(2020, 1, 1).toEpochDay();
+
+    int millisInDay = 24 * 60 * 60 * 1000;
+
+    long randomDay = minDay + random.nextInt(maxDay - minDay);
+
+    long randomTime = random.nextInt(millisInDay);
+
+    map.put("umbrella", umbrella);
+    map.put("name", generateRandomString(40));
+    map.put("value", value);
+    map.put("payload", "........");
+    map.put("date_stamp", randomDay);
+    map.put("time", randomTime);
+    map.put("id", generateRandomString(32));
+
+    return map;
+
+  }
+
+  public String generateRandomString(int length) {
+    if (length < 1) {
+      throw new IllegalArgumentException();
+    }
+
+    StringBuilder sb = new StringBuilder(length);
+    for (int i = 0; i < length; i++) {
+
+      // 0-62 (exclusive), random returns 0-61
+      int rndCharAt = random.nextInt(DATA_FOR_RANDOM_STRING.length());
+      char rndChar = DATA_FOR_RANDOM_STRING.charAt(rndCharAt);
+
+      // debug
+      // System.out.format("%d\t:\t%c%n", rndCharAt, rndChar);
+
+      sb.append(rndChar);
+
+    }
+    return sb.toString();
+  }
+
+
+}
